ecb0ffc51bf46713f7378b4ac1c757f46eaa0318
ningaraddi <mercapto225@gmail.com>
2026-01-18 00:42:10 +0530

    Update source files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { SiLeetcode, SiCodechef } from 'react-icons/si';
 import '../styles/index.css'; // Global styles cover simple footer text
 
 const Footer = () => {
@@ -34,7 +35,8 @@ const Footer = () => {
                 <a href="https://github.com/ningaraddiraddi" target="_blank" rel="noreferrer" style={linkStyle}><FaGithub size={20} /></a>
                 <a href="https://www.linkedin.com/in/ningaraddi-raddi/" target="_blank" rel="noreferrer" style={linkStyle}><FaLinkedin size={20} /></a>
                 {/* Placeholder links provided in prompt or generic */}
-                <a href="#" style={linkStyle}><FaTwitter size={20} /></a>
+                <a href="https://leetcode.com/u/mercapto225/" target="_blank" rel="noreferrer" style={linkStyle}><SiLeetcode size={20} /></a>
+                <a href="https://www.codechef.com/users/ningaraddi" target="_blank" rel="noreferrer" style={linkStyle}><SiCodechef size={20} /></a>
             </div>
             <div style={{ fontFamily: 'var(--font-mono)', fontSize: '12px', color: 'var(--light-slate)' }}>
                 <a href="https://github.com/ningaraddiraddi" target="_blank" rel="noreferrer">
